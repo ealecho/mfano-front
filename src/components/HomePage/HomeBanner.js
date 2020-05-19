e67dc6794524b55f554fb3550af3cfb195b064fd
ealecho <elshore32@gmail.com>
2020-05-19 22:39:32 +0300

    gatsby-plugin-resolve-src

--- src/components/HomePage/HomeBanner.js
+++ src/components/HomePage/HomeBanner.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Banner, BannerTitle, BTPrimary, BTSecondary,  Headline, Headline1} from '../../styles/homeStyles'
+import {Banner, BannerTitle, BTPrimary, BTSecondary,  Headline, Headline1} from 'styles/homeStyles'
 
 
 const HomeBanner = () => {
